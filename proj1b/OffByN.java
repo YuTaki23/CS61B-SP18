ddeda3d8b922608ba65d739f08fe1920c504c211
YuTaki <yutaki23@163.com>
2025-01-03 16:28:53 +0800

    proj1b task5 finished

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,2 +1,13 @@
-public class OffByN {
+public class OffByN implements CharacterComparator{
+    public int result;
+
+    public OffByN(int N) {
+        this.result = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        int diff = x - y;
+        return Math.abs(diff) == result;
+    }
 }
