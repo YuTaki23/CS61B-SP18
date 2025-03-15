cf11912b543c84b63d9da3644351f69f73272712
YuTaki <yutaki23@163.com>
2025-03-15 21:54:03 +0800

    last

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -32,14 +32,28 @@ public class ArrayDeque<T> {
     }
 
     private void resize(int newCapacity) {
-        T[] arr = (T[]) new Object[newCapacity];
-        for (int i = 1; i <= size; i++) {
-            arr[i] = items[(++nextFirst) % this.capacity];
+        T[] a = (T[]) new Object[newCapacity];
+
+        int currentFirst = plus(nextFirst);
+        int currentLast = minus(nextLast);
+
+        if (currentFirst < currentLast) {
+            int length = currentLast - currentFirst + 1;
+            System.arraycopy(items, currentFirst, a, 0, length);
+            nextFirst = newCapacity - 1;
+            nextLast = length;
+        } else {
+            int firstRightCount = capacity - currentFirst;
+            int firstLeftCount = capacity - firstRightCount;
+            System.arraycopy(items, currentFirst, a, 0, firstRightCount);
+            System.arraycopy(items, 0, a, firstRightCount, firstLeftCount);
+
+            nextFirst = newCapacity - 1;
+            nextLast = capacity;
         }
-        this.capacity = capacity;
-        nextFirst = 0;
-        nextLast = size + 1;
-        items = arr;
+
+        capacity = newCapacity;
+        items = a;
     }
 
     public void addFirst(T item) {
@@ -72,13 +86,11 @@ public class ArrayDeque<T> {
         if (size == 0) {
             return;
         }
-        int index = nextFirst;
-        index = plus(index);
-        while (index != nextLast - 1) {
+        int index = plus(nextFirst);
+        while (index != nextFirst) {
             System.out.print(items[index] + " ");
             index = plus(index);
         }
-        System.out.print(items[nextLast - 1]);
     }
 
     public T removeFirst() {
@@ -116,4 +128,42 @@ public class ArrayDeque<T> {
             return items[(nextFirst + 1 + index) % capacity];
         }
     }
+
+//    public static void main(String[] args) {
+//        ArrayDeque<Integer> a = new ArrayDeque<>();
+//        a.addFirst(1);
+//        a.addFirst(2);
+//        a.addFirst(3);
+//        a.addFirst(4);
+//        a.addFirst(5);
+//        a.addFirst(6);
+//        a.addFirst(7);
+//        a.addFirst(8);
+//        a.addFirst(9);
+//        a.addFirst(1);
+//        a.addFirst(2);
+//        a.addFirst(3);
+//        a.addFirst(4);
+//        a.addFirst(5);
+//        a.addFirst(6);
+//        a.addFirst(7);
+//        a.addFirst(8);
+//        a.addFirst(9);
+//        a.printDeque();
+//        System.out.println("--------------------------");
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.removeFirst();
+//        a.printDeque();
+//        System.out.println(a.get(10));
+//    }
 }
